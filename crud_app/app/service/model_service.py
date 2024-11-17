a534d3a87b20b77783da49405a673252cb6785b8
Andy Gatza <andy.gatza@gmail.com>
2024-11-16 23:10:23 -0600

    NCGIA-7 Added regular get and remove methods

--- crud_app/app/service/model_service.py
+++ crud_app/app/service/model_service.py
@@ -8,10 +8,25 @@ from app import schemas
 
 logger.logger.setLevel(logging.INFO)
 
+def get_user(db: Session, brand_name: str) -> schemas.Brand:
+  logger.logger.info('Getting Brand: %s', brand_name)
+  db_brand = crud.brand.get(db=db, id_=brand_name)
+  if db_brand is None:
+    raise fastapi.HTTPException(status_code=fastapi.status.HTTP_404_NOT_FOUND,
+                                detail=f'User with id {brand_name} not found')
+  return schemas.Brand.model_validate(db_brand)
+
 def get_brand_list(db: Session, skip: int = 0, limit: int = 100) -> list[schemas.Brand]:
     logger.logger.info('Getting Brand List...')
     brands = crud.brand.get_multi(db, skip=skip, limit=limit)
     return [schemas.Brand.model_validate(brand) for brand in brands]
     
-    
-    
+def remove_brand(db: Session, brand_name: str) -> schemas.Brand:
+  logger.logger.info('Removing Brand: %s', brand_name)
+  existing_db_brand = crud.brand.get(db, id_=brand_name)
+  if existing_db_brand is None:
+    raise fastapi.HTTPException(status_code=fastapi.status.HTTP_404_NOT_FOUND,
+                                detail=f'User with id {brand_name} not found')
+
+  return schemas.Brand.model_validate(crud.brand.remove(db=db, id_=brand_name))
+

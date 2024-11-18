3dc22efc3b0af9525ae3e41a9cffc57fb868adc6
Andy Gatza <andy.gatza@gmail.com>
2024-11-17 20:00:03 -0600

    NCGIA-10 Added get_by_name() for model

--- app/crud/crud_model.py
+++ app/crud/crud_model.py
@@ -1,11 +1,12 @@
 from app.crud import base
 from app import models
 from app import schemas
+from sqlalchemy.orm import Session
 
 
 class CRUDModel(base.CRUDBase[models.Model, schemas.Model,
                                      schemas.Model]):
-  pass
-
+  def get_by_name(self, db: Session) -> models.TermsAndConditions | None:
+    return (db.query(models.Model.name).first())
 
 model = CRUDModel(models.Model)

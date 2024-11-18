3da51a0fdbb806510a01d9e24a06b30dd6e82342
Andy Gatza <andy.gatza@gmail.com>
2024-11-17 19:53:25 -0600

    NCGIA-10 Added new Model attributes

--- app/service/model_service.py
+++ app/service/model_service.py
@@ -6,24 +6,26 @@ from sqlalchemy.orm import Session
 from app import crud
 from app import schemas
 
+import uuid
+
 logger.logger.setLevel(logging.INFO)
 
 def create_model(db: Session, new_model: schemas.Model) -> schemas.Model:
   logger.logger.info('Creating model: %s', new_model.model_dump_json())
-  existing_db_model = crud.model.get(db=db, id_=new_model.name)
+  existing_db_model = crud.model.get(db=db, id_=new_model.id)
   if existing_db_model is not None:
     raise fastapi.HTTPException(status_code=fastapi.status.HTTP_409_CONFLICT,
-                                detail=f'model with name {new_model.name} already exists')
+                                detail=f'model with id {new_model.id} already exists')
 
   db_model = crud.model.create(db=db, obj_in=new_model)
   return schemas.Model.model_validate(db_model)
 
-def get_model(db: Session, model_name: str) -> schemas.Model:
-  logger.logger.info('Getting model: %s', model_name)
-  db_model = crud.model.get(db=db, id_=model_name)
+def get_model(db: Session, model_id: uuid.UUID) -> schemas.Model:
+  logger.logger.info('Getting model: %s', model_id)
+  db_model = crud.model.get(db=db, id_=model_id)
   if db_model is None:
     raise fastapi.HTTPException(status_code=fastapi.status.HTTP_404_NOT_FOUND,
-                                detail=f'model with name {model_name} not found')
+                                detail=f'model with name {model_id} not found')
   return schemas.Model.model_validate(db_model)
 
 def get_models(db: Session, skip: int = 0, limit: int = 100) -> list[schemas.Model]:
@@ -31,12 +33,12 @@ def get_models(db: Session, skip: int = 0, limit: int = 100) -> list[schemas.Mod
     models = crud.model.get_multi(db, skip=skip, limit=limit)
     return [schemas.Model.model_validate(model) for model in models]
     
-def remove_model(db: Session, model_name: str) -> schemas.Model:
-  logger.logger.info('Removing model: %s', model_name)
-  existing_db_model = crud.model.get(db, id_=model_name)
+def remove_model(db: Session, model_id: uuid.UUID) -> schemas.Model:
+  logger.logger.info('Removing model: %s', model_id)
+  existing_db_model = crud.model.get(db, id_=model_id)
   if existing_db_model is None:
     raise fastapi.HTTPException(status_code=fastapi.status.HTTP_404_NOT_FOUND,
-                                detail=f'model with name {model_name} not found')
+                                detail=f'model with name {model_id} not found')
 
-  return schemas.Model.model_validate(crud.model.remove(db=db, id_=model_name))
+  return schemas.Model.model_validate(crud.model.remove(db=db, id_=model_id))
 
--- test/service/test_model_service.py
+++ test/service/test_model_service.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 from fastapi import HTTPException
 from app import schemas
 from app.service import model_service
-
+import uuid
 
 @pytest.fixture
 def mock_db():
@@ -12,7 +12,7 @@ def mock_db():
 
 @pytest.fixture
 def model_schema():
-    return schemas.Model(name="Testmodel")
+    return schemas.Model(id=uuid.uuid4(), type="Monitor", name="Testmodel", description="Test Monitor")
 
 @pytest.fixture
 def mock_crud():
@@ -25,7 +25,7 @@ def test_create_model_success(mock_db, model_schema, mock_crud):
 
     result = model_service.create_model(mock_db, model_schema)
 
-    mock_crud.get.assert_called_once_with(db=mock_db, id_=model_schema.name)
+    mock_crud.get.assert_called_once_with(db=mock_db, id_=model_schema.id)
     mock_crud.create.assert_called_once_with(db=mock_db, obj_in=model_schema)
     assert result == model_schema
 
@@ -35,54 +35,54 @@ def test_create_model_conflict(mock_db, model_schema, mock_crud):
     with pytest.raises(HTTPException) as exc_info:
         model_service.create_model(mock_db, model_schema)
 
-    mock_crud.get.assert_called_once_with(db=mock_db, id_=model_schema.name)
+    mock_crud.get.assert_called_once_with(db=mock_db, id_=model_schema.id)
     assert exc_info.value.status_code == 409
     assert "already exists" in exc_info.value.detail
 
 def test_get_model_success(mock_db, model_schema, mock_crud):
     mock_crud.get.return_value = model_schema
 
-    result = model_service.get_model(mock_db, model_schema.name)
+    result = model_service.get_model(mock_db, model_schema.id)
 
-    mock_crud.get.assert_called_once_with(db=mock_db, id_=model_schema.name)
+    mock_crud.get.assert_called_once_with(db=mock_db, id_=model_schema.id)
     assert result == model_schema
 
 def test_get_model_not_found(mock_db, model_schema, mock_crud):
     mock_crud.get.return_value = None
 
     with pytest.raises(HTTPException) as exc_info:
-        model_service.get_model(mock_db, model_schema.name)
+        model_service.get_model(mock_db, model_schema.id)
 
-    mock_crud.get.assert_called_once_with(db=mock_db, id_=model_schema.name)
+    mock_crud.get.assert_called_once_with(db=mock_db, id_=model_schema.id)
     assert exc_info.value.status_code == 404
     assert "not found" in exc_info.value.detail
 
 def test_get_models(mock_db, mock_crud):
-    mock_crud.get_multi.return_value = [schemas.Model(name="model1"), schemas.Model(name="model2")]
+    mock_crud.get_multi.return_value = [schemas.Model(id=uuid.uuid4(), type="Monitor", name="Testmodel1", description="Test Monitor1"), schemas.Model(id=uuid.uuid4(), type="Monitor", name="Testmodel2", description="Test Monitor2")]
 
     result = model_service.get_models(mock_db, skip=0, limit=100)
 
     mock_crud.get_multi.assert_called_once_with(mock_db, skip=0, limit=100)
     assert len(result) == 2
-    assert result[0].name == "model1"
-    assert result[1].name == "model2"
+    assert result[0].name == "Testmodel1"
+    assert result[1].name == "Testmodel2"
 
 def test_remove_model_success(mock_db, model_schema, mock_crud):
     mock_crud.get.return_value = model_schema
     mock_crud.remove.return_value = model_schema
 
-    result = model_service.remove_model(mock_db, model_schema.name)
+    result = model_service.remove_model(mock_db, model_schema.id)
 
-    mock_crud.get.assert_called_once_with(mock_db, id_=model_schema.name)
-    mock_crud.remove.assert_called_once_with(db=mock_db, id_=model_schema.name)
+    mock_crud.get.assert_called_once_with(mock_db, id_=model_schema.id)
+    mock_crud.remove.assert_called_once_with(db=mock_db, id_=model_schema.id)
     assert result == model_schema
 
 def test_remove_model_not_found(mock_db, model_schema, mock_crud):
     mock_crud.get.return_value = None
 
     with pytest.raises(HTTPException) as exc_info:
-        model_service.remove_model(mock_db, model_schema.name)
+        model_service.remove_model(mock_db, model_schema.id)
 
-    mock_crud.get.assert_called_once_with(mock_db, id_=model_schema.name)
+    mock_crud.get.assert_called_once_with(mock_db, id_=model_schema.id)
     assert exc_info.value.status_code == 404
     assert "not found" in exc_info.value.detail
